cafa8dfc9872e0b4f81a5da9fbb7580466333ebd
JeovanaKno <knoblauchjeovana6@gmail.com>
2025-10-06 22:54:14 -0400

    Alterações

--- obj/Debug/net8.0/AppVisioLens.AssemblyInfo.cs
+++ obj/Debug/net8.0/AppVisioLens.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppVisioLens")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e52b1511216dce2255f2774a00f85cc93aba3b89")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppVisioLens")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppVisioLens")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
